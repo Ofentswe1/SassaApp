685f2398ae0cfd5816728145dfc409063352922d
LEBOGO OFENTSWE RAMOSHAU <ofentswel@gmail.com>
2018-04-09 21:48:12 +0200

    Login fixed API Login coming soon

--- app/src/main/java/za/co/itechhub/sassaapp/LoginActivity.java
+++ app/src/main/java/za/co/itechhub/sassaapp/LoginActivity.java
@@ -1,9 +1,9 @@
 package za.co.itechhub.sassaapp;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -33,25 +33,31 @@ public class LoginActivity extends AppCompatActivity{
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(idNumber.toString() == "" && password.toString() == ""){
-                    Context context = getApplicationContext();
-                    int duration = Toast.LENGTH_SHORT;
-                    CharSequence text = "Username and Password cannotbe Empty!";
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
+
+                if(TextUtils.isEmpty(password.getText()) && TextUtils.isEmpty(idNumber.getText())){
+                    ToastMessage("All Fields are required!");
+                    idNumber.setError("Id Number Required.");
+                    password.setError("Password Required.");
+                }
+                else if(TextUtils.isEmpty(idNumber.getText())){
+                    ToastMessage("Id Number Required.");
+                    idNumber.setError("Id Number Required.");
+                }
+                else if(TextUtils.isEmpty(password.getText())){
+                    ToastMessage("Password Required.");
+                    password.setError("Password Required.");
                 }
                 else{
-                    Context context = getApplicationContext();
-                    int duration = Toast.LENGTH_SHORT;
-                    CharSequence text = "Login Successful!";
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
                     startActivity(new Intent(getBaseContext(), MainActivity.class));
                     finish();
                 }
-
             }
         });
     }
 
+    public void ToastMessage(CharSequence text) {
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(this, text, duration);
+        toast.show();
+    }
 }
