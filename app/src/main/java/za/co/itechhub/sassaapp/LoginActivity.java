44ef94675ddc51bdd4d9579233291ecf536e6a35
LEBOGO OFENTSWE RAMOSHAU <ofentswel@gmail.com>
2018-04-09 22:49:24 +0200

    The postExecute Inserted with API Link needs to be finished

--- app/src/main/java/za/co/itechhub/sassaapp/LoginActivity.java
+++ app/src/main/java/za/co/itechhub/sassaapp/LoginActivity.java
@@ -1,13 +1,28 @@
 package za.co.itechhub.sassaapp;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * A login screen that offers login via email/password.
@@ -15,10 +30,8 @@ import android.widget.Toast;
 @SuppressWarnings("ALL")
 public class LoginActivity extends AppCompatActivity {
 
-
-    // UI references.
     private EditText idNumber, password;
-
+    private ArrayList<User> users = new ArrayList<>();
     // Toast context and duration
 
     @Override
@@ -33,7 +46,7 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                //new JSONTask().execute("http://192.168.8.106:8000/app/personal/ofentswe/");
                 if (TextUtils.isEmpty(password.getText()) && TextUtils.isEmpty(idNumber.getText())) {
 
                     ToastMessage("All Fields are required!");
@@ -64,5 +77,97 @@ public class LoginActivity extends AppCompatActivity {
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(this, text, duration);
         toast.show();
+    }
+
+    private class JSONTask extends AsyncTask<String, String, String> implements AdapterView.OnItemClickListener {
+
+        @Override
+        protected String doInBackground(String... params) {
+            BufferedReader reader = null;
+            HttpURLConnection connection = null;
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer = new StringBuffer();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+                String jsonObject = buffer.toString();
+                String finalObject = "{" + '"' + "users" + '"' + ": " + jsonObject + "}";
+
+                JSONObject jsonParent = new JSONObject(finalObject);
+                JSONArray jsonArray = jsonParent.getJSONArray("users");
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject object = jsonArray.getJSONObject(i);
+                    String name = object.getString("first_name");
+                    String surname = object.getString("last_name");
+                    String address = object.getString("email");
+                    String username = object.getString("username");
+
+                    System.out.println("name: "+ name+", Surname: "+surname+", address: "+address+", username: "+username);
+                    users.add(new User(name, surname, address, username));
+                }
+                return buffer.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null)
+                    connection.disconnect();
+                try {
+                    if (reader != null)
+                        reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (validateInpuut()) {
+                for (User user : users) {
+                    if(user.getUsername().equals(idNumber.getText().toString())&&
+                            user.getSurname().equals(password.getText().toString())){
+                        Toast.makeText(getBaseContext(),"Access granted",Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(getBaseContext(),MapsActivity.class);
+                        intent.putExtra("_user",user);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+            }
+        }
+
+        public boolean validateInpuut() {
+            if (TextUtils.isEmpty(idNumber.getText())) {
+                idNumber.setError("Required.");
+                return false;
+            } else {
+                idNumber.setError(null);
+            }
+            if (TextUtils.isEmpty(password.getText())) {
+                password.setError("Required.");
+                return false;
+            } else {
+                password.setError(null);
+            }
+            return true;
+        }
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+        }
     }
 }
---
+++ app/src/main/java/za/co/itechhub/sassaapp/User.java
@@ -0,0 +1,53 @@
+package za.co.itechhub.sassaapp;
+
+import java.io.Serializable;
+
+/**
+ * Created by lero on 2018/04/09.
+ */
+
+public class User implements Serializable {
+    String name;
+    String surname;
+    String address;
+    String username;
+
+    public User(String name, String surname, String address, String username) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
